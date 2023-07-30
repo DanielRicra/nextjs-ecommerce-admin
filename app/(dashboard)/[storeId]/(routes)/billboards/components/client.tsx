efdfc5e688a4296dcd8422dad4f1ae844299ab10
DanielRicra <daniel.int@outlook.com>
2023-07-30 11:45:04 -0500

    Update BillboardClient to add table
    - the table renders all the billboards of the specific store.

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,21 +1,34 @@
 "use client";
 
+import { format } from "date-fns";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
 import Heading from "@/components/heading";
 import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
 import { Separator } from "@/components/ui/separator";
+import useStoreBillboards from "@/hooks/useStoreBillboards";
+import { BillboardColumn, columns } from "./columns";
 
 export const BillboardClient = () => {
 	const router = useRouter();
 	const params = useParams();
 
+	const { billboards } = useStoreBillboards(params.storeId as string);
+
+	const formattedBillboards: BillboardColumn[] =
+		billboards?.map((billboard) => ({
+			id: billboard.id,
+			label: billboard.label,
+			createdAt: format(new Date(billboard.createdAt), "MMMM do, yyyy"),
+		})) ?? [];
+
 	return (
 		<>
 			<div className="flex items-center justify-between">
 				<Heading
-					title="Billboards (0)"
+					title={`Billboards (${billboards?.length ?? 0})`}
 					description="Manage billboards for your store"
 				/>
 				<Button
@@ -27,6 +40,12 @@ export const BillboardClient = () => {
 			</div>
 
 			<Separator />
+
+			<DataTable
+				columns={columns}
+				data={formattedBillboards}
+				searchKey="label"
+			/>
 		</>
 	);
 };
