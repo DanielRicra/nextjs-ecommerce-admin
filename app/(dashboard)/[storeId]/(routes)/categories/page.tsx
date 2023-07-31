fba30469551214f542d3ea80321434be526957ae
DanielRicra <daniel.int@outlook.com>
2023-07-30 21:10:03 -0500

    Add CategoriesPage

---
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -0,0 +1,12 @@
+import { CategoryClient } from "./components/client";
+
+const CategoriesPage = () => {
+	return (
+		<div className="flex flex-col">
+			<div className="flex-1 space-y-4 p-8 pt-6">
+				<CategoryClient />
+			</div>
+		</div>
+	);
+};
+export default CategoriesPage;
