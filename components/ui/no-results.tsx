df5d31b2d0b9de5957e4204985e801f1c5d6d184
DanielRicra <daniel.int@outlook.com>
2023-08-02 22:51:00 -0500

    Add no-results and container

---
+++ components/ui/no-results.tsx
@@ -0,0 +1,8 @@
+const NoResults = () => {
+	return (
+		<div className="flex items-center text-xl justify-center h-full w-full text-neutral-500 py-4">
+			No results found
+		</div>
+	);
+};
+export default NoResults;
