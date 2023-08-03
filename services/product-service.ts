3482ac48cd9dd2174168e31797da00ce52f25fdb
DanielRicra <daniel.int@outlook.com>
2023-08-03 12:04:55 -0500

    Update product-service to add get product by id

--- services/product-service.ts
+++ services/product-service.ts
@@ -16,9 +16,29 @@ export const getProducts = async (query: Query): Promise<ProductData[]> => {
 			categoryId: query.categoryId,
 			sizeId: query.sizeId,
 			isFeatured: query.isFeatured,
+			isArchived: false,
 		},
 	});
 
 	const response = await fetch(url);
+
+	if (!response.ok) {
+		throw new Error("Error fetching product");
+	}
+
+	return response.json();
+};
+
+export const getProductById = async (
+	productId: string,
+): Promise<ProductData> => {
+	const response = await fetch(
+		`http://localhost:3000/api/c96aca53-bd8d-4e5a-9310-bc3d334235eb/products/${productId}`,
+	);
+
+	if (!response.ok) {
+		throw new Error("Error fetching product");
+	}
+
 	return response.json();
 };
