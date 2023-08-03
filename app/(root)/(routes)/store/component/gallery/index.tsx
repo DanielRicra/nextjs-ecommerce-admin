147ea39db160e4a1b0782345264e16bab351256c
DanielRicra <daniel.int@outlook.com>
2023-08-03 12:04:26 -0500

    Add gallery component

---
+++ app/(root)/(routes)/store/component/gallery/index.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState } from "react";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { type Swiper as SwiperType } from "swiper/types";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
+
+import "./styles.css";
+
+import { type Image as ImageModel } from "@prisma/client";
+import Image from "next/image";
+
+interface GalleryProps {
+	images: ImageModel[];
+}
+
+const Gallery: React.FC<GalleryProps> = ({ images }) => {
+	const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null);
+
+	return (
+		<div>
+			<div className="mx-auto mt-6 w-full max-w-2xl sm:block lg:max-w-none w-[300px] lg:w-[500px]">
+				<Swiper
+					style={
+						{
+							"--swiper-navigation-color": "#000",
+							"--swiper-pagination-color": "#000",
+						} as React.CSSProperties
+					}
+					spaceBetween={10}
+					navigation={true}
+					thumbs={{ swiper: thumbsSwiper }}
+					modules={[FreeMode, Navigation, Thumbs]}
+					className="w-[300px] lg:w-[500px] aspect-square border rounded-md mb-4 mySwiper2"
+				>
+					{images.map((image) => (
+						<SwiperSlide key={image.id}>
+							<Image
+								src={image.url}
+								fill
+								alt="product image"
+								className="object-cover"
+							/>
+						</SwiperSlide>
+					))}
+				</Swiper>
+
+				<Swiper
+					onSwiper={(swiper) => setThumbsSwiper(swiper)}
+					spaceBetween={10}
+					slidesPerView={4}
+					freeMode={true}
+					watchSlidesProgress={true}
+					modules={[FreeMode, Navigation, Thumbs]}
+					className="h-[120px] py-2 mySwiper"
+				>
+					{images.map((image) => (
+						<SwiperSlide
+							key={image.id}
+							className="border rounded-md w-[25%] aspect-square overflow-hidden"
+						>
+							<Image
+								src={image.url}
+								fill
+								alt="product image"
+								className="block object-cover cursor-pointer"
+							/>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
+		</div>
+	);
+};
+export default Gallery;
