fd8578c36eeccf16f59979005a9320ccd0cdbfb8
DanielRicra <daniel.int@outlook.com>
2023-08-03 22:29:50 -0500

    Add category data interface

--- types.ts
+++ types.ts
@@ -1,8 +1,19 @@
-import type { Category, Color, Image, Product, Size } from "@prisma/client";
+import type {
+	Billboard,
+	Category,
+	Color,
+	Image,
+	Product,
+	Size,
+} from "@prisma/client";
 
 export interface ProductData extends Product {
 	images: Image[];
 	category: Category;
 	size: Size;
 	color: Color;
+}
+
+export interface CategoryData extends Category {
+	billboard: Billboard;
 }
