080a471c2a0aee0461337619d035959e0561cee7
DanielRicra <daniel.int@outlook.com>
2023-08-03 22:26:14 -0500

    Add product modal provider

--- app/(root)/(routes)/store/layout.tsx
+++ app/(root)/(routes)/store/layout.tsx
@@ -1,3 +1,4 @@
+import ProductModalProvider from "@/providers/product-modal-provider";
 import { Urbanist } from "next/font/google";
 import Footer from "./component/footer";
 import Navbar from "./component/store-navbar";
@@ -12,6 +13,7 @@ export const metadata = {
 async function StoreLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className={font.className}>
+			<ProductModalProvider />
 			<Navbar />
 			{children}
 			<Footer />
