516476066fc58607a74041e9d9bbb915aa7c8003
DanielRicra <daniel.int@outlook.com>
2023-08-04 18:16:54 -0500

    Update to show the amount of cart items and redirect to /cart

--- app/(root)/(routes)/store/component/navbar-actions.tsx
+++ app/(root)/(routes)/store/component/navbar-actions.tsx
@@ -4,9 +4,14 @@ import { ShoppingBagIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
+import { useRouter } from "next/navigation";
 
 const NavbarActions = () => {
 	const [isMounted, setIsMounted] = useState(false);
+	const router = useRouter();
+
+	const cart = useCart();
 
 	useEffect(() => {
 		setIsMounted(true);
@@ -18,9 +23,14 @@ const NavbarActions = () => {
 
 	return (
 		<div className="ml-auto flex items-center gap-x-4">
-			<Button className="rounded-full flex gap-2 items-center">
+			<Button
+				className="rounded-full flex gap-2 items-center"
+				onClick={() => router.push("/store/cart")}
+			>
 				<ShoppingBagIcon size={20} color="white" />
-				<span className="text-sm font-medium text-white">0</span>
+				<span className="text-sm font-medium text-white">
+					{cart.items.length}
+				</span>
 			</Button>
 		</div>
 	);
