ca0ef442b92d4e5e8e274377adcd073b3eb54be2
DanielRicra <daniel.int@outlook.com>
2023-08-04 18:14:14 -0500

    Add theme provider to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import ModalProvider from "@/providers/modal-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 import ToastProvider from "@/providers/toast-provider";
 import "./globals.css";
 
@@ -22,9 +23,11 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang="en">
 				<body className={inter.className}>
-					{children}
-					<ToastProvider />
-					<ModalProvider />
+					<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+						{children}
+						<ToastProvider />
+						<ModalProvider />
+					</ThemeProvider>
 				</body>
 			</html>
 		</ClerkProvider>
